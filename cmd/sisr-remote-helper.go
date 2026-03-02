f1e5fc16a99fcad7a7566b29c39964730774479d
Mike Wyatt <wyatt.mike@gmail.com>
2026-03-02 17:52:26 -0400

    add win32 command

--- cmd/sisr-remote-helper.go
+++ cmd/sisr-remote-helper.go
@@ -41,6 +41,7 @@ func main() {
 
 		Commands: []*cli.Command{
 			&uwpCommand,
+			&win32Command,
 		},
 	}
 
@@ -164,6 +165,121 @@ var uwpCommand = cli.Command{
 
 			targetFamily := strings.Split(aumid, "_")[0]
 			srh.KillUwpApp(targetFamily)
+
+			return ctx.Err()
+		case err := <-done:
+			return err
+		}
+	},
+}
+
+var win32Command = cli.Command{
+	Name:      "win32",
+	Usage:     "Launch SISR and a Win32 executable",
+	ArgsUsage: "<exe-path>",
+
+	Flags: []cli.Flag{
+		&cli.BoolWithInverseFlag{
+			Name:  "start-sisr",
+			Usage: "Whether to start SISR automatically",
+			Value: true,
+		},
+
+		&cli.StringFlag{
+			Name:  "sisr-path",
+			Value: "./SISR",
+		},
+
+		&cli.StringFlag{
+			Name: "sisr-config",
+		},
+
+		&cli.StringSliceFlag{
+			Name:  "grep",
+			Usage: "Also consider the app running if any process exe path contains this string (can be specified multiple times)",
+		},
+
+		&cli.StringSliceFlag{
+			Name:  "arg",
+			Usage: "Argument to pass to the executable (can be specified multiple times)",
+		},
+	},
+
+	Action: func(ctx context.Context, cmd *cli.Command) error {
+		exePath := cmd.Args().Get(0)
+		if exePath == "" {
+			return fmt.Errorf("exe path is required")
+		}
+
+		greps := cmd.StringSlice("grep")
+		exeArgs := cmd.StringSlice("arg")
+
+		ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+		defer stop()
+		done := make(chan error, 1)
+
+		shouldStartSisr := cmd.Bool("start-sisr")
+
+		var sisrCmd *exec.Cmd
+		if shouldStartSisr {
+			sisrCmdPath := cmd.String("sisr-path")
+			sisrCmdArgs := []string{
+				"--marker",
+				"--debug",
+				"--log-level", "debug",
+				"--log-file", "sisr-remote-helper-sisr.log",
+			}
+
+			if config := cmd.String("sisr-config"); config != "" {
+				sisrCmdArgs = append(sisrCmdArgs, "--config", config)
+			}
+
+			slog.Info("Starting SISR", slog.Any("command", sisrCmdPath), slog.Any("args", sisrCmdArgs))
+			sisrCmd = exec.Command(sisrCmdPath, sisrCmdArgs...)
+			sisrCmd.Stdout = os.Stdout
+			sisrCmd.Stderr = os.Stderr
+			err := sisrCmd.Start()
+			if err != nil {
+				return fmt.Errorf("failed to start SISR: %w", err)
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+
+		defer func() {
+			if sisrCmd != nil && sisrCmd.Process != nil {
+				slog.Info("Killing SISR helper", slog.Any("pid", sisrCmd.Process.Pid))
+				killCmd := exec.Command("taskkill", "/F", "/T", "/PID", fmt.Sprintf("%d", sisrCmd.Process.Pid))
+				killCmd.Stdout = os.Stdout
+				killCmd.Stderr = os.Stderr
+				killCmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+				if err := killCmd.Run(); err != nil {
+					slog.Warn("Failed to kill SISR process", slog.Any("error", err))
+				}
+
+				time.Sleep(1 * time.Second)
+
+				slog.Info("Running steam://forceinputappid/0")
+				steamForceInputCmd := exec.Command("cmd", "/c", "start", "steam://forceinputappid/0")
+				steamForceInputCmd.Stdout = os.Stdout
+				steamForceInputCmd.Stderr = os.Stderr
+				steamForceInputCmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+				if err := steamForceInputCmd.Run(); err != nil {
+					slog.Warn("Failed to send forceinputappid command", slog.Any("error", err))
+				}
+			}
+		}()
+
+		go func() {
+			slog.Info("Launching app", slog.Any("exe", exePath), slog.Any("args", exeArgs))
+			done <- srh.StartAndWaitForWin32App(exePath, exeArgs, greps)
+		}()
+
+		select {
+		case <-ctx.Done():
+			slog.Warn("Interrupted by user")
+
+			srh.KillWin32App(exePath, greps)
 
 			return ctx.Err()
 		case err := <-done:
